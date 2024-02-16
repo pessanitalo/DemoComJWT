0c4b3b811c9b16b0595003877fc2846243c6714b
italo pessan <italopessan@gmail.com>
2024-02-16 13:51:56 -0300

    classes de dominio
    classes de livro. applicationUser e configuração banco de dados

--- api/Context/DataContext.cs
+++ api/Context/DataContext.cs
@@ -1,6 +1,19 @@
-﻿namespace DemoJWT.Context
+﻿using DemoJWT.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoJWT.Context
 {
-    public class DataContext
+    public class DataContext : IdentityDbContext<ApplicationUser>
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+
+        public DbSet<Livros> Livros { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
---
+++ api/Models/Livros.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoJWT.Models
+{
+    public class Livros
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Autor { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
