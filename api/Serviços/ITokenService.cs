2158ef8c2494a666b637b3cba288eb29f568626a
italo pessan <italopessan@gmail.com>
2024-02-16 13:37:48 -0300

    Token service

---
+++ api/Serviços/ITokenService.cs
@@ -0,0 +1,14 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace DemoJWT.Serviços
+{
+    public interface ITokenService
+    {
+        JwtSecurityToken GenerateAccessToken(IEnumerable<Claim> claims, IConfiguration _config);
+
+        string GenerateRefreshToken();
+
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token, IConfiguration _config);
+    }
+}
---
+++ api/Serviços/TokenService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DemoJWT.Serviços
+{
+    public class TokenService : ITokenService
+    {
+        public JwtSecurityToken GenerateAccessToken(IEnumerable<Claim> claims, IConfiguration _config)
+        {
+            var key = _config.GetSection("JWT").GetValue<string>("SecretKey") ??
+               throw new InvalidOperationException("Invalid secret Key");
+
+            var privateKey = Encoding.UTF8.GetBytes(key);
+
+            var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(privateKey),
+                                     SecurityAlgorithms.HmacSha256Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(_config.GetSection("JWT")
+                                                    .GetValue<double>("TokenValidityInMinutes")),
+                Audience = _config.GetSection("JWT")
+                                  .GetValue<string>("ValidAudience"),
+                Issuer = _config.GetSection("JWT").GetValue<string>("ValidIssuer"),
+                SigningCredentials = signingCredentials
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateJwtSecurityToken(tokenDescriptor);
+            return token;
+        }
+
+        public string GenerateRefreshToken()
+        {
+            var secureRandomBytes = new byte[128];
+
+            using var randomNumberGenerator = RandomNumberGenerator.Create();
+
+            randomNumberGenerator.GetBytes(secureRandomBytes);
+
+            var refreshToken = Convert.ToBase64String(secureRandomBytes);
+
+            return refreshToken;
+        }
+
+        public ClaimsPrincipal GetPrincipalFromExpiredToken(string token, IConfiguration _config)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
