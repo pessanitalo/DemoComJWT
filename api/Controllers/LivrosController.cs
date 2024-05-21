cea10a773aec361888283382bd4225ba6d87c945
italo pessan <italopessan@gmail.com>
2024-05-20 21:55:20 -0300

    Projeto jwt

---
+++ api/Controllers/LivrosController.cs
@@ -0,0 +1,26 @@
+﻿using DemoJWT.Context;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoJWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivrosController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public LivrosController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult Get() 
+        { 
+            var livros = _context.Livros.ToList();
+            return Ok(livros);
+        }
+    }
+}
---
+++ api/DTOs/RegisterModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DemoJWT.DTOs
+{
+    public class RegisterModel
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+}
